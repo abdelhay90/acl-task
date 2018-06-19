e612398c5b40e2c95732e33f3d5fc3f8d54c7d0e
Ahmed Abdelhay <a_abd_elhay@live.com>
2018-06-19 16:31:37 +0200

    update acl library

--- test/acl.test.js
+++ test/acl.test.js
@@ -1,10 +1,78 @@
 import 'babel-polyfill';
 import acl from '../lib/acl';
-const expect = require('chai').expect;
+import RoleChecker from '../lib/acl/lib/RoleChecker';
+import {a, an, check} from '../lib/acl';
+import {PERMISSION_TYPE, ROLE_TYPE, VERBS} from '../lib/acl/constants';
+import {expect} from 'chai';
 
 describe('ACL', () => {
+    acl.createRole(ROLE_TYPE.ADMIN);
+    acl.createRole(ROLE_TYPE.USER);
+    beforeEach(() => {
+
+    });
+
     it('should be defined', function () {
-        console.log(acl);
         expect(acl).to.not.equal(undefined);
+    });
+
+    it('should be singleton classes', function () {
+        expect(() => {
+            let t = new acl();
+        }).to.throw();
+
+        expect(() => {
+            let acl = new RoleChecker();
+        }).to.throw();
+    });
+
+    describe('Roles', function () {
+        it('should add roles', function () {
+            expect(acl.getRole(ROLE_TYPE.ADMIN)).to.not.equal(undefined);
+            expect(acl.getRole(ROLE_TYPE.USER)).to.not.equal(undefined);
+            expect(Object.keys(acl.roles).length).to.equal(2);
+        });
+
+        it('should assign permissions', function () {
+            let permission = an(ROLE_TYPE.ADMIN).can(VERBS.GET).from('/users');
+            expect(permission.verbName).to.equal(VERBS.GET);
+            expect(permission.type).to.equal(PERMISSION_TYPE.GRANT);
+            expect(permission.path).to.equal('/users');
+            expect(acl.getRole(ROLE_TYPE.ADMIN)._permissions.length).to.equal(1);
+        });
+
+        it('should assign conditional permissions', function () {
+            let permission = a(ROLE_TYPE.USER).can(VERBS.POST).to('/users/:userId/articles').when((params, user) =>
+                user.id === params.userId);
+            expect(permission.verbName).to.equal(VERBS.POST);
+            expect(permission.type).to.equal(PERMISSION_TYPE.GRANT);
+            expect(permission.path).to.equal('/users/:userId/articles');
+            expect(acl.getRole(ROLE_TYPE.USER)._permissions.length).to.equal(1);
+        });
+    });
+
+    describe('Role Checker', function () {
+        it('should check if the role is valid', function () {
+            expect(check.if(ROLE_TYPE.ADMIN)).to.not.equal(undefined);
+            expect(check.if(ROLE_TYPE.ADMIN).can(VERBS.GET)).to.not.equal(undefined);
+            expect(() => {
+                check.if(ROLE_TYPE.GUEST)
+            }).to.throw();
+            expect(() => {
+                check.if(ROLE_TYPE.ADMIN).can(VERBS.POST)
+            }).to.throw();
+            expect(check.if(ROLE_TYPE.ADMIN).can(VERBS.GET).from('/admins')).to.equal(false);
+            expect(check.if(ROLE_TYPE.ADMIN).can(VERBS.GET).from('/users')).to.equal(true);
+            expect(
+                check
+                    .if(ROLE_TYPE.USER)
+                    .can(VERBS.POST)
+                    .to('/users/:userId/articles')
+                    .when([
+                        {userId: '15'},
+                        {id: '15'}
+                    ])
+            ).to.equal(true);
+        });
     });
 });
